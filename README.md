aa06e79d647897904cecd3bf757574e3bb82967e
Domodiak <56079789+Domodiak@users.noreply.github.com>
2024-07-15 10:59:10 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Welcome to Domodiak's Pages
+
+Hey there! 👋 Welcome to my Github Pages repository. This is the place where I host my personal website and projects. Feel free to explore and discover what I've been up to.
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import avatar from './assets/logo192.png'
+
+function App() {
+  return (
+    <>
+      <h1>🚧 Work in progress 🚧</h1>
+      <img src={avatar} alt="Domodiak's avatar"></img>
+    </>
+  )
+}
+
+export default App
